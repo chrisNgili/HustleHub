b3ff6f7c35c966b39359a9b660aeb80258ecc37b
chrisngili <ngilichristopher@gmail.com>
2025-04-21 18:53:53 +0300

    developing the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import { NavLink } from "react-router-dom";
 function NavBar(){
     return(
         <nav>
-            
+            <NavLink to="/" className="nav-link">Home</NavLink>
+            <NavLink to="/addjob" className="nav-link">Add job</NavLink>
+            <NavLink to="/login" className="nav-link">Login</NavLink>
+            <NavLink to="/About" className="nav-link">About</NavLink>
         </nav>
     )
 }
+
+export default NavBar
