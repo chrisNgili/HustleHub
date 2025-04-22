0996e21b53f3ff9cec9e4d4e000885ab9dbd34b6
chrisngili <ngilichristopher@gmail.com>
2025-04-22 10:28:00 +0300

    creating routes

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Home from "./pages/Home";
+import AddJob from "./pages/AddJob";
+import About from "./pages/About";
+import JobProfile from "./pages/JobProfile";
+
+
+const routes =[]
+
+export default routes
