01ca06473bd6bd4dd6224536f00a96f3b55eef7a
chrisngili <ngilichristopher@gmail.com>
2025-04-22 13:14:30 +0300

    adding about content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,18 @@ import NavBar from "../components/NavBar";
 
 
 function About() {
-
+    return(
+        <div>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <h1>About us</h1>
+                <p>HustleHub is a professional platform enabling employers to display open positions to prospective job seekers, streamlining hiring and applications. This user-friendly system supports career growth through opportunity discovery and progress tracking.
+                </p>
+            </main>
+        </div>
+    )
 }
 
 export default About;
