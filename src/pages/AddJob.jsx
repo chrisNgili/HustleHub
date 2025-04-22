90145de405a8998e3a8a4aa32827e6efce4e44e4
chrisngili <ngilichristopher@gmail.com>
2025-04-22 13:26:09 +0300

    adding POST on addjob file

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,8 +1,37 @@
 import NavBar from "../components/NavBar";
 import React,{ useState } from "react";
 
-function AddJob(){
+function AddJob( {onAddJob} ){
+    const [title, setTitle] = useState("");
+    const [company, setCompany] = useState("");
+    const [slots, setSlots] = useState("");
+    const [deadline, setDeadline] = useState("");
+    const [contact, setContact] = useState("");
+    const [status, setStatus] = useState("");
+    const [description, setDescription] = useState("");
 
+    function handleSubmit(e){
+        e.preventDefault()
+        const newJob = {
+            title,
+            company,
+            slots,
+            deadline,
+            contact,
+            status,
+            description
+        };
+        fetch("https://hustlehub-rc5s.onrender.com/opportunities",{
+            method:"POST",
+            headers:{
+                "Content-type" : "application/json",
+            },
+            body: JSON.stringify(newJob),
+        })
+        .then((r) => r.json())
+        .then((data) =>onAddJob(data))
+    }
+    return
 }
 
 export default AddJob;
